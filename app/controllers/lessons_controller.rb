4a474e0af4b235daa016f41d59ace84b98f203d3
Kellen McCarvel <kellen.mccarvel@gmail.com>
2015-01-02 20:52:07 +0000

    lesson 27

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user!
+before_action :require_current_user_enrollment, :only => [:show]
+
 	def show
 	end
 
@@ -7,4 +10,12 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson = Lesson.find(params[:id])
 	end
+
+	def require_current_user_enrollment
+		if current_user.enrolled_in?(@course) == false
+			redirect_to course_path(current_lesson, :course_id), :alert => 'You need to enroll in this class to gain access.'
+		end
+	end
+
+
 end
