096c2d9d76eba8371989df664e28167cb49d796d
Kellen McCarvel <kellen.mccarvel@gmail.com>
2015-01-05 13:52:02 +0000

    lesson 33

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resource :dashboard, :only => [:show]
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
   resources :lessons, :only => [:show]
   namespace :instructor do 
     resources :lessons, :only => [:update]
-    resources :sections, :only => [:update]
-    resources :sections, :only => [] do
+    resources :sections, :only => [:update] do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
